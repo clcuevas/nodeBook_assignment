78611ec712cd4cdf0750225eed36b911bb542150
Claudia Cuevas <omordn@gmail.com>
2015-05-04 09:25:45 -0700

    add final files, working

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-//require the http module
 var http = require('http');
 var url = require('url');
 
 function start(route, handle) {
   function onRequest(request, response) {
-    var postData = '';
+    //var postData = '';
     var pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname + '  received');
 
-    request.setEncoding('utf-8');
+    route(handle, pathname, response, request);
+    // request.setEncoding('utf-8');
 
-    request.addListener('data', function(postDataChunk) {
-      postData += postDataChunk;
-      console.log('Received POST data chunk ' + postDataChunk + ' .');
-    });
+    // request.addListener('data', function(postDataChunk) {
+    //   postData += postDataChunk;
+    //   console.log('Received POST data chunk ' + postDataChunk + ' .');
+    // });
 
-    request.addListener('end', function() {
-      route(handle, pathname, response, postData);
-    });
+    // request.addListener('end', function() {
+    //   route(handle, pathname, response, postData);
+    // });
   }
-  
+
   http.createServer(onRequest).listen(3000);
   console.log('Server has started');
 }
